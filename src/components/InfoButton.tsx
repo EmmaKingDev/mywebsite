8146de0e5adc6daffc25a388e3c5973cf911bba3
Emma King <emma@emmaking.dev>
2025-02-06 08:37:44 +0200

    changed hosting

--- src/components/InfoButton.tsx
+++ src/components/InfoButton.tsx
@@ -1,12 +1,14 @@
-import { useState } from 'react';
-import './Poem.css';
+import React, { useState } from "react";
+import "./Poem.css";
 
 export default function InfoButton() {
   return (
-    <a href="https://github.com/EmmaKingDev/mywebsite" target="_blank">
-    <button className="infoButton">
-      What is this?
-    </button>
+    <a
+      href="https://github.com/EmmaKingDev/mywebsite"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <button className="infoButton">What is this?</button>
     </a>
   );
 }
--- src/components/PoemDisplay.tsx
+++ src/components/PoemDisplay.tsx
@@ -1,28 +1,27 @@
-import { useState } from 'react';
-import usePoem from '../hooks/usePoem';
-import './Poem.css';
-import { usePageRefresh } from '../hooks/usePageRefresh';
+import React, { useState } from "react";
+import usePoem from "../hooks/usePoem";
+import "./Poem.css";
+import { usePageRefresh } from "../hooks/usePageRefresh";
 
 export default function PoemDisplay() {
+  const [poemData, loading] = usePoem();
+  const refreshPage = usePageRefresh();
 
-	const [poemData, loading] = usePoem();
-	const refreshPage = usePageRefresh();
+  if (loading) {
+    return <div className="spinner" />;
+  }
 
-	if (loading) {
-		return <div className="spinner"/>
-	}
-
-	if (poemData) {
-		return (
-			<>
-				<ul className="poem">
-					{poemData.map(line => <li>{line}</li> )}
-				</ul>
-				<button onClick={refreshPage}>
-				New
-				</button>
-			</>
-		);
-	}
-	return <div>Error API oh no</div>
+  if (poemData && Array.isArray(poemData)) {
+    return (
+      <>
+        <ul className="poem">
+          {poemData.map((line: string, index: number) => (
+            <li key={index}>{line}</li>
+          ))}
+        </ul>
+        <button onClick={refreshPage}>New</button>
+      </>
+    );
+  }
+  return <div>Error API oh no</div>;
 }
